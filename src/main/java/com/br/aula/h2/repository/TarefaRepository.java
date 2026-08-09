713cdb55f2c1c10d558ea2b275134b1da25f7528
dvizioon <danielmartinsjob@gmail.com>
2026-08-09 18:22:21 -0300

    Remove unused application files including H2Application, CorsConfig, HealthController, TarefaController, Tarefa model, TarefaRepository, and associated test classes to streamline the project structure.

--- src/main/java/com/br/aula/h2/repository/TarefaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.br.aula.h2.repository;
-
-import com.br.aula.h2.model.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
